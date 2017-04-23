96cb7736afb2507482bf51291b04599949babb11
Inas Hyatt <inas.raheema@gmail.com>
2017-04-22 23:07:25 -0700

    added view garage

--- parking_garage.py
+++ parking_garage.py
@@ -5,6 +5,8 @@ class Garage:
 		self.moto_spots = moto_spots
 		self.comp_spots = comp_spots
 		self.large_spots = large_spots
+		self.capacity = self.rows * (self.moto_spots + self.comp_spots + self.large_spots)
+		self.spaces_taken = 0 
 		self.garage = {}
 		self.populate_garage()
 
@@ -16,6 +18,8 @@ class Garage:
 
 
 	def park_vehicle(self, vehicle):
+		if self.capacity == self.spaces_taken:
+			return vehicle + " cannot be parked in this garage."
 
 		if vehicle == "motorcycle":
 			return self.find_moto_spots(vehicle)
@@ -32,6 +36,7 @@ class Garage:
 		for i in range(self.rows):
 			if self.garage[i][0] > 0:
 				self.garage[i][0] -= 1
+				self.spaces_taken += 1
 				parked = True
 				return vehicle + " has been parked."
 		if parked == False:
@@ -42,6 +47,7 @@ class Garage:
 		for i in range(self.rows):
 			if self.garage[i][1] > 0:
 				self.garage[i][1] -= 1
+				self.spaces_taken += 1
 				parked = True
 				return vehicle + " has been parked."
 		if parked == False:
@@ -53,11 +59,13 @@ class Garage:
 			if vehicle == "bus":
 				if self.garage[i][2] >= 5:
 					self.garage[i][2] -= 5
+					self.spaces_taken += 5
 					parked = True
 					return vehicle + " has been parked."
 			else:
 				if self.garage[i][2] > 0:
 					self.garage[i][2] -= 1
+					self.spaces_taken += 1
 					parked = True
 					return vehicle + " has been parked."
 
@@ -65,14 +73,32 @@ class Garage:
 			return vehicle + " will not fit in this garage."
 
 
+	def view_lot(self):
+		print "Spaces Available:"
+		for i in range(self.rows):
+			print "level: " + str(i) + ": " + "Motorcycle Spaces " + str(self.garage[i][0]) + " Compact Spaces "+ str(self.garage[i][1]) + " Large Spots " + str(self.garage[i][2])
+
+
 
 
 if __name__ == '__main__':
-	lot = Garage(5, 2, 2 , 5)
+	lot = Garage(2, 2, 2 , 5)
+	print lot.park_vehicle("motorcycle")
+	print lot.park_vehicle("car")
+	print lot.park_vehicle("bus")
+	print lot.park_vehicle("motorcycle")
+	print lot.park_vehicle("car")
+	print lot.park_vehicle("bus")
+	print lot.park_vehicle("motorcycle")
+	print lot.park_vehicle("car")
+	print lot.park_vehicle("bus")
+	print lot.park_vehicle("motorcycle")
+	print lot.park_vehicle("car")
+	print lot.park_vehicle("bus")
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
 	print lot.park_vehicle("bus")
 
-	print lot.garage
+	print lot.view_lot()
 
 
