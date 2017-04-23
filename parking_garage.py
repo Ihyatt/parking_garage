a8f72709e556ae6d02d4a03156e3d4ee2269cc37
Inas Hyatt <inas.raheema@gmail.com>
2017-04-23 13:36:19 -0700

    updated base cases

--- parking_garage.py
+++ parking_garage.py
@@ -2,18 +2,19 @@
 class Garage: 
 	def __init__(self, rows, moto_spots, comp_spots, large_spots):
 		self.rows = rows
-		self.moto_spots = moto_spots
-		self.comp_spots = comp_spots
-		self.large_spots = large_spots
-		self.capacity = self.rows * (self.moto_spots + self.comp_spots + self.large_spots)
+		self.open_moto_spots = self.rows * moto_spots
+		self.open_comp_spots = self.rows * comp_spots
+		self.open_large_spots = self.rows * large_spots
+
+		self.capacity =(self.open_moto_spots + self.open_comp_spots + self.open_large_spots)
 		self.spaces_taken = 0 
 		self.garage = {}
-		self.populate_garage()
+		self.populate_garage(moto_spots, comp_spots, large_spots)
 
-	def populate_garage(self):
+	def populate_garage(self, moto_spots, comp_spots, large_spots):
 
 		for i in range(self.rows):
-			self.garage[i] = [self.moto_spots, self.comp_spots, self.large_spots]
+			self.garage[i] = [moto_spots, comp_spots, large_spots]
 
 
 
@@ -32,40 +33,54 @@ class Garage:
 
 
 	def find_moto_spots(self, vehicle):
+		if self.open_moto_spots == 0:
+			return self.find_comp_spots(vehicle)
+
+
 		parked = False
 		for i in range(self.rows):
 			if self.garage[i][0] > 0:
 				self.garage[i][0] -= 1
 				self.spaces_taken += 1
+				self.open_moto_spots -= 1
 				parked = True
 				return vehicle + " has been parked."
 		if parked == False:
 			return self.find_comp_spots(vehicle)
 
 	def find_comp_spots(self, vehicle):
+		if self.open_comp_spots == 0:
+			return self.find_large_spots(vehicle)
+
 		parked = False
 		for i in range(self.rows):
 			if self.garage[i][1] > 0:
 				self.garage[i][1] -= 1
 				self.spaces_taken += 1
+				self.open_comp_spots -= 1
 				parked = True
 				return vehicle + " has been parked."
 		if parked == False:
 			return self.find_large_spots(vehicle)
 
 	def find_large_spots(self, vehicle):
+		if self.open_large_spots == 0: 
+			return vehicle + " will not fit in this garage."
+
 		parked = False
 		for i in range(self.rows):
 			if vehicle == "bus":
 				if self.garage[i][2] >= 5:
 					self.garage[i][2] -= 5
 					self.spaces_taken += 5
+					self.open_large_spots -= 5
 					parked = True
 					return vehicle + " has been parked."
 			else:
 				if self.garage[i][2] > 0:
 					self.garage[i][2] -= 1
 					self.spaces_taken += 1
+					self.open_large_spots -= 1
 					parked = True
 					return vehicle + " has been parked."
 
@@ -83,21 +98,22 @@ class Garage:
 
 if __name__ == '__main__':
 	lot = Garage(2, 2, 2 , 5)
+	print lot.view_lot()
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
-	print lot.park_vehicle("bus")
+
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
-	print lot.park_vehicle("bus")
+
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
-	print lot.park_vehicle("bus")
+
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
-	print lot.park_vehicle("bus")
+
 	print lot.park_vehicle("motorcycle")
 	print lot.park_vehicle("car")
-	print lot.park_vehicle("bus")
+
 
 	print lot.view_lot()
 
